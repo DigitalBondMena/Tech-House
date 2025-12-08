2ba519a387429b5ed5108201272bcef1e9b0df6e
DigitalBondMena <bonder@digitalbondmena.com>
2025-12-08 10:58:49 +0200

    Adding home service  section

--- src/app/features/home/home-projects/home-projects.ts
+++ src/app/features/home/home-projects/home-projects.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { SectionTitle } from '../../../shared/components/section-title/section-title';
 
 @Component({
   selector: 'app-home-projects',
-  imports: [],
+  imports: [SectionTitle],
   templateUrl: './home-projects.html',
   styleUrl: './home-projects.css'
 })
 export class HomeProjects {
+  //! section title data
+  projectsTitle = "مشاريعنا";
 
 }
