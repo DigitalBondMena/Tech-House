9372b4b4b3c8c28ca4ff681ad310b199a131b64f
DigitalBondMena <bonder@digitalbondmena.com>
2025-12-08 11:45:45 +0200

    Adding scroll navbar styling

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  isScrolled = signal(false);
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.scrollY;
+    this.isScrolled.set(scrollPosition > 100);
+  }
 }
