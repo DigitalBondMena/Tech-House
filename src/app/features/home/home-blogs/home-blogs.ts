8bb393f6ed6e68ed6ea9ac8815c09002d7a4dc47
DigitalBondMena <bonder@digitalbondmena.com>
2025-12-08 16:08:40 +0200

    Adding home blogs section

--- src/app/features/home/home-blogs/home-blogs.ts
+++ src/app/features/home/home-blogs/home-blogs.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { SectionTitle } from '../../../shared/components/section-title/section-title';
+import { AppButton } from '../../../shared/components/app-button/app-button';
 
 @Component({
   selector: 'app-home-blogs',
-  imports: [],
+  imports: [SectionTitle, AppButton],
   templateUrl: './home-blogs.html',
   styleUrl: './home-blogs.css'
 })
 export class HomeBlogs {
+  //! section title data
+  projectsTitle = " المقالات";
 
+  //! button data
+  btnText = "مقالات اكثر";
+
+
+  activeCard: number = 2;
+
+  //! method to set active card
+  setActive(cardNumber: number) {
+    this.activeCard = cardNumber;
+  }
 }
