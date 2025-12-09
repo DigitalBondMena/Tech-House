65049716a8feca2864720cebfb176012ae594bbc
DigitalBondMena <bonder@digitalbondmena.com>
2025-12-09 15:36:45 +0200

    home

--- src/app/shared/components/contact-us-sec/contact-us-sec.ts
+++ src/app/shared/components/contact-us-sec/contact-us-sec.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { SectionTitle } from '../section-title/section-title';
+import { AppButton } from '../app-button/app-button';
 
 @Component({
   selector: 'app-contact-us-sec',
-  imports: [],
+  imports: [SectionTitle, AppButton],
   templateUrl: './contact-us-sec.html',
   styleUrl: './contact-us-sec.css'
 })
 export class ContactUsSec {
+  //! section title data
+  projectsTitle = "تواصل معنا";
+
+  //! button data
+  btnText = "ارسال ";
 
 }
